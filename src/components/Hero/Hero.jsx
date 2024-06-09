f6b82381c00248ec1180c89650e52cb075658530
nauvaltr <naufaltrisubaktilbb123@gmail.com>
2024-06-09 08:06:48 +0700

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Naufal</h1>
+        <p className={styles.description}>
+        I'm a UI/UX Designer,Graphic Designer, Front-End Web Developer, Front-End Mobile Developer, and System Analyst  .
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/FotoProfile.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
